c1eb9d80ac8a16e4aa9983b46485a4ef1bd26ad8
LukaGluscic <bozic1387@gmail.com>
2024-06-07 17:25:10 +0200

    Arhi

--- src/components/Arhitektura.vue
+++ src/components/Arhitektura.vue
@@ -1,5 +1,19 @@
 <template>
-    <!-- Content for Arhitektura page -->
+ <div class="tittle">
+ <h2 class="title-first">Arhitektura</h2>
+ <p class="tittle-desc">
+  Arhitekti pružaju usluge dizajna i planiranja za unapređenje, modernizaciju, uređenje i preuređenje vašeg prostora. Pronađite iskusne stručnjake za vaš sljedeći arhitektonski projekt danas!
+ </p>
+ </div>
+ <div class="posao">
+  <h3 class="subheading">Trebaš majstora?</h3>
+  <button class="button">Naruči uslugu</button>
+  <p class="paragraph">
+    Projektiranje arhitektonskih prostora predstavlja proces preuređenja, poboljšanja ili modernizacije određenog dijela ili cijelog stambenog ili poslovnog objekta. Bilo da je riječ o arhitektonskom dizajnu kuhinje, kupaonice, potkrovlja, tavana ili vanjskog prostora, ovaj postupak obuhvata niz aktivnosti kojima se unapređuje funkcionalnost i estetika prostora.
+
+  </p>
+  <img class="adapt" src ="img\586199_orig.jpg">
+ </div>
   </template>
   
   <script>
@@ -9,6 +23,105 @@
   </script>
   
   <style scoped>
-  /* ... */
-  </style>
-  
+.title {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  margin-bottom: 50px;
+}
+
+.title-first{
+  font-size: 90px;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+
+.title-desc {
+  font-size: 25px;
+  color: #666;
+  margin-bottom: 60px;
+  margin-left: 300px;
+  margin-right: 300px;
+  flex-grow: 1;
+}
+
+.subheading {
+  font-size: 36px;
+  margin-bottom: 20px;
+}
+
+.button {
+  font-size: 18px;
+  font-weight: bold;
+  border: none;
+  border-radius: 20px;
+  padding: 10px 20px;
+  cursor: pointer;
+  background-color: #cbb3ff;
+}
+
+.button:hover {
+  background-color: #a2a2ff;
+}
+
+.paragraph {
+  font-size: 18px;
+  color: #666;
+  margin-top: 40px;
+  margin-left: 300px;
+  margin-right: 300px;
+  flex-grow: 1;
+}
+
+.adapt{
+  width: 40%;
+  height: auto;
+}
+
+@media (max-width: 1200px) {
+  .title-desc{
+    margin-right: 200px;
+    margin-left: 200px;
+  }
+  .paragraph{
+    margin-right: 200px;
+    margin-left: 200px
+  }
+}
+
+@media (max-width: 992px) {
+  .title-desc{
+    margin-right: 100px;
+    margin-left: 100px;
+  }
+  .paragraph{
+    margin-right: 100px;
+    margin-left: 100px
+  }
+  .adapt{
+    width: 60%;
+    height: auto;
+  }
+}
+
+@media (max-width: 768px) {
+  .title-first{
+    font-size: 70px;
+  }
+  .title-desc{
+    margin-right: 50px;
+    margin-left: 50px;
+  }
+  .paragraph{
+    margin-right: 50px;
+    margin-left: 50px
+  }
+  .adapt{
+    width: 80%;
+    height: auto;
+  }
+}
+
+
+</style>
